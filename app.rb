13a0dbf30ef7efaa1b2c1b08b02418920208de31
Chris Mytton <chrismytton@gmail.com>
2015-10-29 14:57:55 +0000

    Only use the last 64k of rake output
    GitHub Pull Request bodies seem to be limited to ~64k characters.

--- app.rb
+++ app.rb
@@ -29,6 +29,8 @@ class RebuilderJob
     end
     api_key = ERB::Util.url_encode(ENV['MORPH_API_KEY'])
     output = output.gsub(api_key, 'REDACTED')
+    # Only use last 64k of output
+    output = output[-64_000..-1] || output
     title = "#{country_slug}: refresh data"
     body = "Automated data refresh for #{country_slug} - #{legislature_slug}" \
       "\n\n#### Output\n\n```\n#{output.uncolorize}\n```"
