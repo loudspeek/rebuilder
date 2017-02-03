d0f06b1e8f77f10425b7af8ca1f10c448f2fb064
Chris Mytton <chrismytton@gmail.com>
2017-02-03 12:41:54 +0100

    Add route for url-based rebuild of a specific source
    This route can be used by scrapers as the webhook url, which allows us
    to have the webhook only trigger a rebuild for the source that the
    scraper provides.

--- test/app_test.rb
+++ test/app_test.rb
@@ -63,4 +63,46 @@ describe 'Rebuilder' do
       last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=gender-balance\n"
     end
   end
+
+  describe 'url-based rebuilds using country and legislature slugs' do
+    describe 'without a source' do
+      before { post '/rebuild/Thailand/National-Legislative-Assembly' }
+
+      it 'is successful' do
+        last_response.status.must_equal 200
+      end
+
+      it 'queues one job' do
+        RebuilderJob.jobs.size.must_equal 1
+      end
+
+      it 'has the correct arguments' do
+        RebuilderJob.jobs.first['args'].must_equal ['Thailand', 'National-Legislative-Assembly', nil]
+      end
+
+      it 'confirms rebuild in response body' do
+        last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=\n"
+      end
+    end
+
+    describe 'with a source' do
+      before { post '/rebuild/Thailand/National-Legislative-Assembly/gender-balance' }
+
+      it 'is successful' do
+        last_response.status.must_equal 200
+      end
+
+      it 'queues one job' do
+        RebuilderJob.jobs.size.must_equal 1
+      end
+
+      it 'has the correct arguments' do
+        RebuilderJob.jobs.first['args'].must_equal %w(Thailand National-Legislative-Assembly gender-balance)
+      end
+
+      it 'confirms rebuild in response body' do
+        last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=gender-balance\n"
+      end
+    end
+  end
 end
