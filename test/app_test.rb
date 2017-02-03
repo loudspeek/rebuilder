434008690d40c584bf80db49928993742a453182
Chris Mytton <chrismytton@gmail.com>
2017-02-03 11:56:47 +0100

    Switch tests to use spec-style assertions
    This style of assertion has become our preferred approach in newer
    projects, so switching these tests over for consistency.

--- test/app_test.rb
+++ test/app_test.rb
@@ -8,19 +8,19 @@ describe 'Rebuilder' do
     before { post '/Thailand/National_Legislative_Assembly' }
 
     it 'is successful' do
-      assert_equal 200, last_response.status
+      last_response.status.must_equal 200
     end
 
     it 'queues one job' do
-      assert_equal 1, RebuilderJob.jobs.size
+      RebuilderJob.jobs.size.must_equal 1
     end
 
     it 'has the correct arguments' do
-      assert_equal ['Thailand', 'National-Legislative-Assembly', nil], RebuilderJob.jobs.first['args']
+      RebuilderJob.jobs.first['args'].must_equal ['Thailand', 'National-Legislative-Assembly', nil]
     end
 
     it 'confirms rebuild in response body' do
-      assert_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=\n", last_response.body
+      last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=\n"
     end
   end
 
@@ -28,19 +28,19 @@ describe 'Rebuilder' do
     before { post '/', country: 'Thailand', legislature: 'National-Legislative-Assembly' }
 
     it 'is successful' do
-      assert_equal 200, last_response.status
+      last_response.status.must_equal 200
     end
 
     it 'queues one job' do
-      assert_equal 1, RebuilderJob.jobs.size
+      RebuilderJob.jobs.size.must_equal 1
     end
 
     it 'has the correct arguments' do
-      assert_equal ['Thailand', 'National-Legislative-Assembly', nil], RebuilderJob.jobs.first['args']
+      RebuilderJob.jobs.first['args'].must_equal ['Thailand', 'National-Legislative-Assembly', nil]
     end
 
     it 'confirms rebuild in response body' do
-      assert_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=\n", last_response.body
+      last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=\n"
     end
   end
 
@@ -48,19 +48,19 @@ describe 'Rebuilder' do
     before { post '/', country: 'Thailand', legislature: 'National-Legislative-Assembly', source: 'gender-balance' }
 
     it 'is successful' do
-      assert_equal 200, last_response.status
+      last_response.status.must_equal 200
     end
 
     it 'queues one job' do
-      assert_equal 1, RebuilderJob.jobs.size
+      RebuilderJob.jobs.size.must_equal 1
     end
 
     it 'has the correct arguments' do
-      assert_equal %w(Thailand National-Legislative-Assembly gender-balance), RebuilderJob.jobs.first['args']
+      RebuilderJob.jobs.first['args'].must_equal %w(Thailand National-Legislative-Assembly gender-balance)
     end
 
     it 'confirms rebuild in response body' do
-      assert_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=gender-balance\n", last_response.body
+      last_response.body.must_equal "Queued rebuild for country=Thailand legislature=National-Legislative-Assembly source=gender-balance\n"
     end
   end
 end
