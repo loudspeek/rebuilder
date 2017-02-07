8dd96bb664049b910763f8df837e4b3d58e1abb5
Chris Mytton <chrismytton@gmail.com>
2017-02-07 18:23:43 +0100

    Add ExternalCommand class
    This class encapsulates running an external command and gathering up the
    output.

---
+++ test/external_command_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../lib/external_command'
+
+describe 'ExternalCommand' do
+  describe 'running a simple command' do
+    subject { ExternalCommand.new(command: 'echo Hello, world.').run }
+
+    it 'returns the expected output' do
+      subject.output.must_equal "Hello, world.\n"
+    end
+
+    it 'is considered to be successful' do
+      subject.success?.must_equal true
+    end
+  end
+
+  describe 'running a command with environment variables set' do
+    subject { ExternalCommand.new(command: 'echo Hello, $name.', env: { 'name' => 'Bob' }).run }
+
+    it 'returns the expected output' do
+      subject.output.must_equal "Hello, Bob.\n"
+    end
+
+    it 'is considered to be successful' do
+      subject.success?.must_equal true
+    end
+  end
+
+  describe 'running an external command that fails' do
+    subject { ExternalCommand.new(command: 'echo fail; exit 1').run }
+
+    it 'returns the expected output' do
+      subject.output.must_equal "fail\n"
+    end
+
+    it 'is considered to be a failure' do
+      subject.success?.must_equal false
+    end
+  end
+end
